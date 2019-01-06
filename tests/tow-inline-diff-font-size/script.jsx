926a76a656a0bb0c9c984125e6cc55443977988e
army8735 <army8735@qq.com>
2019-01-06 15:17:37 +0800

    add tests

---
+++ tests/tow-inline-diff-font-size/script.jsx
@@ -0,0 +1,7 @@
+karas.render(
+  <canvas width="360" height="360">
+    <span>hello</span>
+    <span style={{fontSize:36}}>world</span>
+  </canvas>,
+  '#test'
+);
---
+++ tests/tow-inline/script.jsx
@@ -0,0 +1,7 @@
+karas.render(
+  <canvas width="360" height="360">
+    <span>hello</span>
+    <span>world</span>
+  </canvas>,
+  '#test'
+);

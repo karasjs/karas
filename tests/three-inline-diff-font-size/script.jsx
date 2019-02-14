6292d21b758678f41a1019d368d96a07326c2f23
army8735 <army8735@qq.com>
2019-02-14 09:36:53 +0800

    add test

---
+++ tests/three-inline-diff-font-size/script.jsx
@@ -0,0 +1,8 @@
+karas.render(
+  <canvas width="360" height="360">
+    <span style={{fontSize:12}}>hello</span>
+    <span style={{fontSize:36}}>world</span>
+    <span>!</span>
+  </canvas>,
+  '#test'
+);

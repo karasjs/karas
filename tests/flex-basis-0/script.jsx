a4f3281534c5fb3bac5192f761f2e59e779b2220
army8735 <army8735@qq.com>
2019-09-13 13:28:19 +0800

    test: fix

--- tests/flex-basis-0/script.jsx
+++ tests/flex-basis-0/script.jsx
@@ -1,6 +1,6 @@
 karas.render(
   <canvas width="360" height="360">
-    <div style={{display:'flex',ju}}>
+    <div style={{display:'flex',justifyContent:'space-between'}}>
       <span style={{flexBasis:0}}>1</span>
       <span style={{flexBasis:0}}>2</span>
       <span style={{flexBasis:0}}>3</span>

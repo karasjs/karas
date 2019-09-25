2f43f93d701fde0cae40defa8630f4c197a6a509
army8735 <army8735@qq.com>
2019-09-25 09:35:44 +0800

    test: fix test

--- tests/border-dotted-block/script.jsx
+++ tests/border-dotted-block/script.jsx
@@ -1,14 +1,14 @@
 karas.render(
   <canvas width="360" height="360">
-      <div style={{border:'10px dashed #F00'}}>1</div>
-      <div style={{borderTop:'10px dashed #00F'}}>1</div>
-      <div style={{borderRight:'10px dashed #00F'}}>1</div>
-      <div style={{borderLeft:'10px dashed #00F'}}>1</div>
-      <div style={{borderBottom:'10px dashed #00F'}}>1</div>
-      <div style={{borderLeft:'10px dashed #F00',borderBottom:'10px dashed #00F'}}>1</div>
-      <div style={{borderRight:'10px dashed #F00',borderBottom:'10px dashed #00F'}}>1</div>
-      <div style={{borderLeft:'10px dashed #F00',borderTop:'10px dashed #0F0'}}>1</div>
-      <div style={{borderRight:'10px dashed #F00',borderTop:'10px dashed #0F0'}}>1</div>
+      <div style={{border:'10px dotted #F00'}}>1</div>
+      <div style={{borderTop:'10px dotted #00F'}}>1</div>
+      <div style={{borderRight:'10px dotted #00F'}}>1</div>
+      <div style={{borderLeft:'10px dotted #00F'}}>1</div>
+      <div style={{borderBottom:'10px dotted #00F'}}>1</div>
+      <div style={{borderLeft:'10px dotted #F00',borderBottom:'10px dotted #00F'}}>1</div>
+      <div style={{borderRight:'10px dotted #F00',borderBottom:'10px dotted #00F'}}>1</div>
+      <div style={{borderLeft:'10px dotted #F00',borderTop:'10px dotted #0F0'}}>1</div>
+      <div style={{borderRight:'10px dotted #F00',borderTop:'10px dotted #0F0'}}>1</div>
   </canvas>,
   '#test'
 );

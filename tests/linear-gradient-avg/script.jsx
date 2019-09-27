df83f8d77efbf01b9f63f97d5725efe753f00716
army8735 <army8735@qq.com>
2019-09-27 10:51:00 +0800

    test: typo

--- tests/linear-gradient-avg/script.jsx
+++ tests/linear-gradient-avg/script.jsx
@@ -1,8 +1,8 @@
 karas.render(
-  <svg width="360" height="360">
+  <canvas width="360" height="360">
     <div style={{marginBottom:5,height:50,background:`linear-gradient(#0F0, #00F)`}}/>
     <div style={{marginBottom:5,height:50,background:`linear-gradient(#0F0, #000, #00F)`}}/>
     <div style={{marginBottom:5,height:50,background:`linear-gradient(#0F0, #000, #F00, #00F)`}}/>
-  </svg>,
+  </canvas>,
   '#test'
 );

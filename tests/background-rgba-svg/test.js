b3d1f560ece6a88f80dd38017740f4886ea11ac7
army8735 <army8735@qq.com>
2019-09-27 09:35:17 +0800

    test: add defs

--- tests/background-rgba-svg/test.js
+++ tests/background-rgba-svg/test.js
@@ -6,7 +6,7 @@ module.exports = {
     browser
       .url('file://' + path.join(__dirname, 'index.html'))
       .waitForElementVisible('body', 1000)
-      .assert.value('input', '{"bb":[],"type":"dom","children":[{"bb":[{"type":"item","tagName":"rect","props":[["x",0],["y",0],["width",24],["height",18.3984375],["fill","rgba(255,0,0,0.5)"]]}],"type":"dom","children":[{"type":"text","children":[{"type":"item","tagName":"text","props":[["x",0],["y",14.484375],["fill","#000"],["font-size","16px"]],"content":"123"}]}]}]}')
+      .assert.value('input', '{"bb":[],"type":"dom","children":[{"bb":[{"type":"item","tagName":"rect","props":[["x",0],["y",0],["width",24],["height",18.3984375],["fill","rgba(255,0,0,0.5)"]]}],"type":"dom","children":[{"type":"text","children":[{"type":"item","tagName":"text","props":[["x",0],["y",14.484375],["fill","#000"],["font-size","16px"]],"content":"123"}]}]}],"defs":[]}')
       .end();
   }
 };
--- tests/linear-gradient-avg-svg/script.jsx
+++ tests/linear-gradient-avg-svg/script.jsx
@@ -1,9 +1,9 @@
 let o = karas.render(
-  <canvas width="360" height="360">
+  <svg width="360" height="360">
     <div style={{marginBottom:5,height:50,background:`linear-gradient(#0F0, #00F)`}}/>
     <div style={{marginBottom:5,height:50,background:`linear-gradient(#0F0, #000, #00F)`}}/>
     <div style={{marginBottom:5,height:50,background:`linear-gradient(#0F0, #000, #F00, #00F)`}}/>
-  </canvas>,
+  </svg>,
   '#test'
 );
 var input = document.querySelector('#base64');

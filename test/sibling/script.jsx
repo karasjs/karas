6cb85ee77eba9b7f18e3ad6856f74a568efad3f9
army8735 <army8735@qq.com>
2019-10-17 15:09:06 +0800

    test: sibling

--- test/sibling/script.jsx
+++ test/sibling/script.jsx
@@ -1,11 +1,12 @@
+function click(o) {
+  let [a, b] = o.children;
+  document.querySelector('input').value = a.prev + ',' + (a.next === b) + ',' + (a === b.prev) + ',' + b.next;
+}
+
 let o = karas.render(
-  <canvas width="360" height="360">
+  <canvas width="360" height="360" onClick={() => click(o)}>
     <div>a</div>
     <div>b</div>
   </canvas>,
   '#test'
 );
-
-let [a, b] = o.children;
-
-document.querySelector('input').value = a.prev + ',' + (a.next === b) + ',' + (a === b.prev) + ',' + b.next;

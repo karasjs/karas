d51f4e62581391b81f6c73b038db936891bc0c6f
army8735 <army8735@qq.com>
2019-10-20 21:45:46 +0800

    test: setState为异步

--- test/component-setstate-svg/test.js
+++ test/component-setstate-svg/test.js
@@ -9,6 +9,7 @@ module.exports = {
       .assert.value('input', '{"bb":[],"children":[{"bb":[],"children":[{"type":"text","children":[{"type":"item","tagName":"text","props":[["x",0],["y",14.484375],["fill","#000"],["font-family","arial"],["font-weight",400],["font-style","normal"],["font-size","16px"]],"content":"123"}]}],"transform":[],"type":"dom"}],"transform":[],"type":"dom","defs":[]}')
       .moveToElement('svg', 10, 10)
       .mouseButtonClick(0)
+      .pause(100)
       .assert.value('input', '{"bb":[],"children":[{"bb":[],"children":[{"type":"text","children":[{"type":"item","tagName":"text","props":[["x",0],["y",14.484375],["fill","#000"],["font-family","arial"],["font-weight",400],["font-style","normal"],["font-size","16px"]],"content":"456"}]}],"transform":[],"type":"dom"}],"transform":[],"type":"dom","defs":[]}')
       .end();
   }

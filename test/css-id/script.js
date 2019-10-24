83d0f5db1d3a516770dd5ee6088b24332457fbf3
army8735 <army8735@qq.com>
2019-10-24 10:04:13 +0800

    chore: remove script.js

--- test/css-id/script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var css = {
-  "#a": {
-    "_v": [[0, ["color", "#F00"]]],
-    "_p": [1, 0, 0]
-  }
-};
-karas.render(karas.createVd("canvas", [["width", "360"], ["height", "360"], ["css", css]], [karas.createVd("div", [["id", "a"]], ["123"])]), '#test');

19585504524ee82f6397dc2e364a1ba6c8c9276f
army8735 <army8735@qq.com>
2019-10-24 10:45:21 +0800

    test: canvas读取不到img使用base64

--- test/img/script.jsx
+++ test/img/script.jsx
@@ -1,6 +1,6 @@
 let o = karas.render(
   <canvas width="360" height="360">
-    <img src="../image.png"/>
+    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkAQMAAABKLAcXAAAABlBMVEX1CAD////Q9ly2AAAAhUlEQVQ4y93TMQ6AIAwF0BIHRo/AUTyaHM2jeARGBoNSkvqNkBASh8YO/rzR9kOqZjllth8pmqc8OSgRWSgSGSjkEL3D53C3KM8sSiwrOlhTW5FlRIFFbe1Fa0v85RyWL3IqNPwPnb109onNd2+Ea+LSzRYg0J66WWhd1Ui0Vc/L+Vaa5gJ+8ifpfqdy4gAAAABJRU5ErkJggg=="/>
   </canvas>,
   '#test'
 );

a9ab4d0043783732f0afe681273a013ee34ee57e
army8735 <army8735@qq.com>
2019-10-25 16:54:31 +0800

    test: add img-transform-parent

---
+++ test/img-transform-parent-svg/script.jsx
@@ -0,0 +1,12 @@
+let o = karas.render(
+  <svg width="360" height="360">
+    <div style={{transform:'translateX(100px)'}}>
+      <img src="../image.png"/>
+    </div>
+  </svg>,
+  '#test'
+);
+o.on(karas.Event.KARAS_REFRESH, function() {
+  var input = document.querySelector('#base64');
+  input.value = JSON.stringify(o.virtualDom);
+});
---
+++ test/img-transform-parent-svg/test.js
@@ -0,0 +1,12 @@
+var path = require('path');
+var fs = require('fs');
+
+module.exports = {
+  'init': function(browser) {
+    browser
+      .url('file://' + path.join(__dirname, 'index.html'))
+      .waitForElementVisible('body', 1000)
+      .assert.value('input', '{"bb":[],"children":[{"bb":[],"children":[{"bb":[],"children":[{"type":"img","tagName":"image","props":[["xlink:href","../image.png"],["x",0],["y",0],["width",100],["height",100]]}],"transform":[],"type":"dom"}],"transform":[["matrix","1,0,0,1,100,0"]],"type":"dom"}],"transform":[],"type":"dom","defs":[]}')
+      .end();
+  }
+};

4d1b8319d946b365802fce4ab7babc7b0d942010
army8735 <army8735@qq.com>
2019-11-12 11:21:48 +0800

    test: 帧数变化影响defs生成uuid变化不可测试 #19

--- test/animate-linear-gradient-svg/script.jsx
+++ test/animate-linear-gradient-svg/script.jsx
@@ -18,5 +18,5 @@ let animation = t.animate([
 });
 animation.on(karas.Event.KARAS_ANIMATION_FINISH, () => {
   let input = document.querySelector('input');
-  input.value = JSON.stringify(o.virtualDom);
+  input.value = JSON.stringify(o.virtualDom).replace(/,"uuid":"karas-defs-\d+-\d+"/g, '');
 });

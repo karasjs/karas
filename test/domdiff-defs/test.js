1fb7c77977f7bb5b1bb5c4353e4b91acc7a76394
army8735 <army8735@qq.com>
2019-11-13 14:04:36 +0800

    test: #19 #20

--- test/domdiff-defs/test.js
+++ test/domdiff-defs/test.js
@@ -6,7 +6,7 @@ module.exports = {
     browser
       .url('file://' + path.join(__dirname, 'index.html'))
       .waitForElementVisible('body', 1000)
-      .assert.value('input', '{"bb":[],"children":[{"bb":[],"children":[{"type":"item","tagName":"circle","props":[["cx",50],["cy",50],["r",50],["fill","url(#karas-defs-0-0)"],["stroke","#000"],["stroke-width",0]]}],"transform":[],"type":"geom"}],"transform":[],"opacity":1,"type":"dom","defs":[{"tagName":"radialGradient","props":[["cx",50],["cy",50],["r",70.71067811865476]],"stop":[["#0F0",0],["rgb(0,0,0)",1]],"uuid":"karas-defs-0-0"}]}')
+      .assert.value('input', '{"bb":[],"children":[{"bb":[],"children":[{"type":"item","tagName":"circle","props":[["cx",50],["cy",50],["r",50],["fill","url(#karas-defs-0-0)"],["stroke","#000"],["stroke-width",0]]}],"transform":[],"opacity":1"opacity":1,"type":"geom"}],"transform":[],"opacity":1,"type":"dom","defs":[{"tagName":"radialGradient","props":[["cx",50],["cy",50],["r",70.71067811865476]],"stop":[["#0F0",0],["rgb(0,0,0)",1]],"uuid":"karas-defs-0-0"}]}')
       .end();
   }
 };
--- test/geom-ellipse-rg-svg/test.js
+++ test/geom-ellipse-rg-svg/test.js
@@ -6,7 +6,7 @@ module.exports = {
     browser
       .url('file://' + path.join(__dirname, 'index.html'))
       .waitForElementVisible('body', 1000)
-      .assert.value('input', '{"bb":[],"children":[{"bb":[],"children":[{"type":"item","tagName":"ellipse","props":[["cx",50],["cy",25],["rx",50],["ry",25],["fill","url(#karas-defs-0-0)"],["stroke","#000"],["stroke-width",1]]}],"transform":[],"type":"geom"},{"bb":[],"children":[{"type":"item","tagName":"ellipse","props":[["cx",50],["cy",75],["rx",25],["ry",12.5],["fill","url(#karas-defs-0-1)"],["stroke","#000"],["stroke-width",1]]}],"transform":[],"type":"geom"}],"transform":[],"opacity":1,"type":"dom","defs":[{"tagName":"radialGradient","props":[["cx",50],["cy",25],["r",55.90169943749474]],"stop":[["#F00",0],["#00F",1]],"uuid":"karas-defs-0-0"},{"tagName":"radialGradient","props":[["cx",50],["cy",75],["r",103.07764064044152]],"stop":[["#F00",0],["#00F",1]],"uuid":"karas-defs-0-1"}]}')
+      .assert.value('input', '{"bb":[],"children":[{"bb":[],"children":[{"type":"item","tagName":"ellipse","props":[["cx",50],["cy",25],["rx",50],["ry",25],["fill","url(#karas-defs-0-0)"],["stroke","#000"],["stroke-width",1]]}],"transform":[]"opacity":1,"type":"geom"},{"bb":[],"children":[{"type":"item","tagName":"ellipse","props":[["cx",50],["cy",75],["rx",25],["ry",12.5],["fill","url(#karas-defs-0-1)"],["stroke","#000"],["stroke-width",1]]}],"transform":[]"opacity":1,"type":"geom"}],"transform":[],"opacity":1,"type":"dom","defs":[{"tagName":"radialGradient","props":[["cx",50],["cy",25],["r",55.90169943749474]],"stop":[["#F00",0],["#00F",1]],"uuid":"karas-defs-0-0"},{"tagName":"radialGradient","props":[["cx",50],["cy",75],["r",103.07764064044152]],"stop":[["#F00",0],["#00F",1]],"uuid":"karas-defs-0-1"}]}')
       .end();
   }
 };

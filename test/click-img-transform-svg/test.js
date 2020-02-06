652a94908d1938f974447750cb549c0d50f9d38f
army8735 <army8735@qq.com>
2020-02-06 15:15:06 +0800

    test: 修复test #28

--- test/click-img-transform-svg/test.js
+++ test/click-img-transform-svg/test.js
@@ -6,7 +6,7 @@ module.exports = {
     browser
       .url('file://' + path.join(__dirname, 'index.html'))
       .waitForElementVisible('body', 1000)
-      .assert.value('input', '{"bb":[],"children":[{"bb":[],"children":[{"type":"img","tagName":"image","props":[["xlink:href","../image.png"],["x",0],["y",0],["width",0],["height",0]]}],"transform":[["matrix","1,0,0,1,100,100"]],"opacity":1,"type":"dom"}],"opacity":1,"type":"dom","defs":[]}')
+      .assert.value('input', '{"bb":[],"children":[{"bb":[],"children":[{"type":"img","tagName":"image","props":[["xlink:href","../image.png"],["x",0],["y",0],["width",0],["height",0]]}],"opacity":1,"transform":"matrix(1,0,0,1,100,100)","type":"dom"}],"opacity":1,"type":"dom","defs":[]}')
       .moveToElement('svg', 101, 101)
       .mouseButtonClick(0)
       .assert.value('input', '0')

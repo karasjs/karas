8bd6daa3aa2a325cd9b7ff9402b03f5a1e03561b
army8735 <army8735@qq.com>
2020-02-18 22:48:56 +0800

    test: pause200 bgi load

--- test/background-image/script.jsx
+++ test/background-image/script.jsx
@@ -4,3 +4,8 @@ karas.render(
   </canvas>,
   '#test'
 );
+setTimeout(() => {
+  let canvas = document.querySelector('canvas');
+  let input = document.querySelector('#base64');
+  input.value = canvas.toDataURL();
+}, 200);

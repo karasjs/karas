f0511aeb99ebf905516c5888f155d7f8240ebb57
army8735 <army8735@qq.com>
2020-02-18 23:32:41 +0800

    test

--- test/background-image/script.jsx
+++ test/background-image/script.jsx
@@ -4,8 +4,3 @@ karas.render(
   </canvas>,
   '#test'
 );
-setTimeout(() => {
-  let canvas = document.querySelector('canvas');
-  let input = document.querySelector('#base64');
-  input.value = canvas.toDataURL();
-}, 200);

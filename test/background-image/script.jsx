89158c14745623214bd48ec987ef49cac2b397e0
army8735 <army8735@qq.com>
2020-02-18 23:03:52 +0800

    test

--- test/background-image/script.jsx
+++ test/background-image/script.jsx
@@ -1,6 +1,6 @@
 karas.render(
   <canvas width="360" height="360">
-    <div style={{height:'100%',background:'url(../image.png)'}}>123</div>
+    <div style={{height:'100%',background:'url(../image.png)'}}/>
   </canvas>,
   '#test'
 );

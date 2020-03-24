7285fd448553963069bb54e673d3c5395ca11f57
army8735 <army8735@qq.com>
2020-03-24 13:42:04 +0800

    test

--- test/background-image/script.jsx
+++ test/background-image/script.jsx
@@ -1,6 +1,12 @@
-karas.render(
+let o = karas.render(
   <canvas width="360" height="360">
     <div style={{height:'100%',background:'url(../image.png)'}}/>
   </canvas>,
   '#test'
 );
+
+o.on('refresh', () => {
+  let canvas = document.querySelector('canvas');
+  let input = document.querySelector('#base64');
+  input.value = canvas.toDataURL();
+});

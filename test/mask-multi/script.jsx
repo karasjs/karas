c03d5f1222c1ee3692b9291f6531f5da7bf56a61
army8735 <army8735@qq.com>
2020-04-16 19:46:03 +0800

    test: #53

--- test/mask-multi/script.jsx
+++ test/mask-multi/script.jsx
@@ -16,7 +16,7 @@ karas.render(
         [1, 1],
         [0, 1]
       ]}
-      style={{position:'absolute',left:60,top:0,width:10,height:10,strokeWidth:0,fill:'#EEE'}}/>
+      style={{position:'absolute',left:60,top:10,width:10,height:10,strokeWidth:0,fill:'#EEE'}}/>
     <div
       ref="t"
       style={{position:'absolute',left:5,top:10,width:100,height:100,background:'#F00'}}>123</div>

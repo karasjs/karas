8b1c4c0559e31501916b75b841bbb99393b114ae
army8735 <army8735@qq.com>
2020-08-03 18:43:41 +0800

    test: cp的ref

--- test/component-ref/script.jsx
+++ test/component-ref/script.jsx
@@ -8,6 +8,10 @@ class Component extends karas.Component {
   click() {
     this.setState({
       text: this.ref.test.tagName,
+    }, function() {
+      let canvas = document.querySelector('canvas');
+      let input = document.querySelector('#base64');
+      input.value = canvas.toDataURL();
     });
   }
   render() {

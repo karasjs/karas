1eebe88223595d3550cac5fa886266c5c6d4252c
army8735 <army8735@qq.com>
2020-08-17 14:30:10 +0800

    test: 解决json倒序问题

--- test/domdiff-cache-svg/test.js
+++ test/domdiff-cache-svg/test.js
@@ -6,7 +6,7 @@ module.exports = {
     browser
       .url('file://' + path.join(__dirname, 'index.html'))
       .waitForElementVisible('body', 1000)
-      .assert.value('input', '{"bb":[],"children":[{"bb":[],"children":[{"type":"text","children":[{"type":"item","tagName":"text","props":[["x",0],["y",14.484375],["fill","rgba(0,0,0,1)"],["font-family","arial"],["font-weight",400],["font-style","normal"],["font-size","16px"]],"content":"123"}]}],"opacity":1,"type":"dom"},{"cache":true,"bb":[],"children":[{"type":"text","children":[{"type":"item","tagName":"text","props":[["x",26.71875],["y",14.484375],["fill","rgba(0,0,0,1)"],["font-family","arial"],["font-weight",400],["font-style","normal"],["font-size","16px"]],"content":"456"}],"cache":true}],"opacity":1,"type":"dom"}],"opacity":1,"type":"dom","defs":[]}')
+      .assert.value('input', '{"bb":[],"children":[{"bb":[],"children":[{"type":"text","children":[{"type":"item","tagName":"text","props":[["x",0],["y",14.484375],["fill","rgba(0,0,0,1)"],["font-family","arial"],["font-weight",400],["font-style","normal"],["font-size","16px"]],"content":"123"}]}],"opacity":1,"type":"dom"},{"bb":[],"children":[{"type":"text","children":[{"type":"item","tagName":"text","props":[["x",26.71875],["y",14.484375],["fill","rgba(0,0,0,1)"],["font-family","arial"],["font-weight",400],["font-style","normal"],["font-size","16px"]],"content":"456"}],"cache":true}],"opacity":1,"type":"dom","cache":true}],"opacity":1,"type":"dom","defs":[],"cache":true}')
       .end();
   }
 };

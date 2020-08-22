2866b8df4675bb387ee528c10ff1070bca1ff6e7
army8735 <army8735@qq.com>
2020-08-22 14:38:47 +0800

    test: typo #81

--- test/domdiff-mask-svg/script.jsx
+++ test/domdiff-mask-svg/script.jsx
@@ -1,6 +1,9 @@
 karas.render(
   <svg width="360" height="360">
     <div>looooooooooooooong</div>
+    <div
+      ref="t"
+      style={{position:'absolute',left:5,top:10,width:100,height:100,background:'#F00'}}>123</div>
     <$polygon
       mask="true"
       points={[
@@ -9,15 +12,15 @@ karas.render(
         [0, 1]
       ]}
       style={{position:'absolute',left:0,top:0,width:100,height:100,strokeWidth:0,fill:'#EEE'}}/>
-    <div
-      ref="t"
-      style={{position:'absolute',left:5,top:10,width:100,height:100,background:'#F00'}}>123</div>
   </svg>,
   '#test'
 );
 let o = karas.render(
   <svg width="360" height="360">
     <div>looooooooooooooong</div>
+    <div
+      ref="t"
+      style={{position:'absolute',left:5,top:10,width:100,height:100,background:'#F00'}}>123</div>
     <$polygon
       mask="true"
       points={[
@@ -26,9 +29,6 @@ let o = karas.render(
         [0, 1]
       ]}
       style={{position:'absolute',left:0,top:0,width:100,height:100,strokeWidth:0,fill:'#333'}}/>
-    <div
-      ref="t"
-      style={{position:'absolute',left:5,top:10,width:100,height:100,background:'#F00'}}>123</div>
   </svg>,
   '#test'
 );

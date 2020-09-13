787d3c6eba8061874f347f1527a302761ea2873d
army8735 <army8735@qq.com>
2020-09-13 10:48:11 +0800

    test: pointer-events #89

--- test/pointer-events-svg/script.jsx
+++ test/pointer-events-svg/script.jsx
@@ -1,7 +1,7 @@
 var input = document.querySelector('#base64');
 let o = karas.render(
   <svg width="360" height="360">
-    <div onClick={e => input.value = 1}>123</div>
+    <div style={{ pointerEvents: 'none' }} onClick={e => input.value = 1}>123</div>
   </svg>,
   '#test'
 );

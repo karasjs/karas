9c56659a733a0f936be71bab10322a508f6a4099
army8735 <army8735@qq.com>
2020-10-15 17:49:19 +0800

    test: add

---
+++ test/cache-total-total-rotate/script.jsx
@@ -0,0 +1,8 @@
+let o = karas.render(
+  <canvas width="360" height="360" cache={true}>
+    <div style={{position:'relative',background:'#F00',padding:10}}>
+      <span style={{rotate:45,padding:10,background:'#00F'}}>123</span>
+    </div>
+  </canvas>,
+  '#test'
+);

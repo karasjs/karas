01ee597ff45db73941ba5165b2d0f044cf239d10
army8735 <army8735@qq.com>
2020-10-16 10:13:06 +0800

    test: add

--- test/cache-children/script.jsx
+++ test/cache-children/script.jsx
@@ -1,8 +1,8 @@
 let o = karas.render(
   <canvas width="360" height="360" cache={true}>
     <div style={{position:'relative',background:'#F00',padding:10}}>
-      <span style={{position:'relative',padding:10,background:'#0F0'}}>123</span>
-      <span style={{position:'relative',padding:10,background:'#00F'}}>456</span>
+      <span style={{padding:10,background:'#0F0'}}>123</span>
+      <span style={{padding:10,background:'#00F'}}>456</span>
     </div>
   </canvas>,
   '#test'

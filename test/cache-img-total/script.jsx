1089d304f1a4a27e403940856f0a240dad0512a5
army8735 <army8735@qq.com>
2020-10-26 14:49:31 +0800

    test: img的cache

--- test/cache-img-total/script.jsx
+++ test/cache-img-total/script.jsx
@@ -6,3 +6,7 @@ let o = karas.render(
   </canvas>,
   '#test'
 );
+o.on('refresh', function() {
+  let input = document.querySelector('#base64');
+  input.value = document.querySelector('canvas').toDataURL();
+});

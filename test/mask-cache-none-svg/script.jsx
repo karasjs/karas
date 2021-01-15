51086032e2a081523f4e83f6fe46bea29900fcb2
army8735 <army8735@qq.com>
2021-01-15 15:15:36 +0800

    test: 0.48.2，<REPAINT时svg和cacheCanvas渲染需重置level为NONE

--- test/mask-cache-none-svg/script.jsx
+++ test/mask-cache-none-svg/script.jsx
@@ -1,6 +1,6 @@
 let o = karas.render(
   <svg width="360" height="360">
-    <div ref="n">looooooooooooooong</div>
+    <div ref="o">
     <div
       ref="t"
       style={{position:'absolute',left:5,top:10,width:100,height:100,background:'#F00'}}>123</div>
@@ -13,6 +13,7 @@ let o = karas.render(
         [0,1]
       ]}
       style={{position:'absolute',left:0,top:0,width:100,height:100,strokeWidth:0,fill:'#EEE'}}/>
+    </div>
   </svg>,
   '#test'
 );
@@ -20,13 +21,9 @@ var input = document.querySelector('#base64');
 o.ref.t.updateStyle({
   translateX: 10,
 }, function() {
-  o.ref.m.updateStyle({
-    translateX: 5,
+  o.ref.o.updateStyle({
+    translateX: 1,
   }, function() {
-    o.ref.t.updateStyle({
-      translateX: 0,
-    }, function() {
-      input.value = document.querySelector('svg').innerHTML;
-    });
+    input.value = document.querySelector('svg').innerHTML;
   });
 });

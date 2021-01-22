ecebe2dd4381377a9eac97e026256108fae5bd97
army8735 <army8735@qq.com>
2021-01-22 10:21:36 +0800

    test: 普通无变更情况布局下的block的margin合并 #129

--- test/geom-rect-svg/test.js
+++ test/geom-rect-svg/test.js
@@ -6,7 +6,7 @@ module.exports = {
     browser
       .url('file://' + path.join(__dirname, 'index.html'))
       .waitForElementVisible('body', 1000)
-      .assert.value('input', '{"bb":[],"children":[{"bb":[],"children":[{"type":"item","tagName":"path","props":[["d","M2,2L102,2L102,52L2,52L2,2"],["fill","none"],["stroke","rgba(255,0,0,1)"],["stroke-width",1]]}],"visibility":"visible","type":"geom"},{"bb":[],"children":[{"type":"item","tagName":"path","props":[["d","M2,56L102,56L102,106L2,106L2,56"],["fill","none"],["stroke","rgba(0,0,0,1)"],["stroke-width",1]]}],"visibility":"visible","type":"geom"}],"visibility":"visible","type":"dom","defs":[]}')
+      .assert.value('input', '{"bb":[],"children":[{"bb":[],"children":[{"type":"item","tagName":"path","props":[["d","M2,2L102,2L102,52L2,52L2,2"],["fill","none"],["stroke","rgba(255,0,0,1)"],["stroke-width",1]]}],"visibility":"visible","type":"geom"},{"bb":[],"children":[{"type":"item","tagName":"path","props":[["d","M2,54L102,54L102,104L2,104L2,54"],["fill","none"],["stroke","rgba(0,0,0,1)"],["stroke-width",1]]}],"visibility":"visible","type":"geom"}],"visibility":"visible","type":"dom","defs":[]}')
       .end();
   }
 };

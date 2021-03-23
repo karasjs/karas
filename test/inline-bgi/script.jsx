66dd1b12a58f1b4bf1e07abb34567675eeb1aa73
army8735 <army8735@qq.com>
2021-03-23 20:15:45 +0800

    test: #147

--- test/inline-bgi/script.jsx
+++ test/inline-bgi/script.jsx
@@ -1,4 +1,4 @@
-karas.render(
+let o = karas.render(
   <canvas width="360" height="360">
     <div style={{margin:10,width:50,background:'#CCC'}}>
       <span style={{background:'url(../image.png) no-repeat center'}}>22222<strong style={{fontSize:50,padding:3}}>33</strong>2</span>
@@ -6,3 +6,7 @@ karas.render(
   </canvas>,
   '#test'
 );
+o.on('refresh', function() {
+  let input = document.querySelector('#base64');
+  input.value = document.querySelector('canvas').toDataURL();
+});

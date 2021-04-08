a6dd8719a46ab1e3c492d559ecb90e6a7b50ed33
army8735 <army8735@qq.com>
2021-04-08 11:11:15 +0800

    chore: unused

--- test/flex-margin-padding/script.jsx
+++
@@ -1,7 +0,0 @@
-karas.render(
-  <canvas width="360" height="360">
-    <div style={{background:'#F00'}}>1</div>
-    <div style={{margin:10,padding:100,background:'#F00'}}>1</div>
-  </canvas>,
-  '#test'
-);

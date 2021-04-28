658365352d01c12a692ec1a76837f262fd0cbde9
army8735 <army8735@qq.com>
2021-04-28 15:36:00 +0800

    test: 去除cache时四个方向上的1px扩展，无需而且增加计算复杂度

--- test/cache-total-children/script.jsx
+++
@@ -1,9 +0,0 @@
-let o = karas.render(
-  <canvas width="360" height="360" cache={true}>
-    <div style={{position:'absolute',background:'#F00',padding:10}}>
-      <span style={{position:'relative',display:'inlineBlock',padding:10,background:'#0F0'}}>123</span>
-      <span style={{position:'relative',display:'inlineBlock',padding:10,background:'#00F'}}>456</span>
-    </div>
-  </canvas>,
-  '#test'
-);

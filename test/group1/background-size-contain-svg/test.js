f5c6fd8b4ca9b4bbbfcf9db7463d87de1a67848e
army8735 <army8735@qq.com>
2021-07-27 15:08:57 +0800

    test: path

--- test/group1/background-size-contain-svg/test.js
+++ test/group1/background-size-contain-svg/test.js
@@ -6,7 +6,7 @@ module.exports = {
     browser
       .url('file://' + path.join(__dirname, 'index.html'))
       .waitForElementVisible('body', 1000)
-      .assert.value('input', '<svg width="360" height="360"><defs></defs><g></g><g><g visibility="visible"><g><path d="M0,0L360,0L360,120L0,120L0,0" fill="rgba(0,0,0,1)"></path><image xlink:href="../image.png" x="0" y="0" width="100" height="100"></image></g><g></g></g><g visibility="visible"><g><path d="M0,120L360,120L360,170L0,170L0,120" fill="rgba(0,255,0,1)"></path><image xlink:href="../image.png" x="0" y="120" width="100" height="100" transform="matrix(0.5,0,0,0.5,0,60)"></image></g><g></g></g><g visibility="visible"><g><path d="M0,170L50,170L50,290L0,290L0,170" fill="rgba(0,0,255,1)"></path><image xlink:href="../image.png" x="0" y="170" width="100" height="100" transform="matrix(0.5,0,0,0.5,0,85)"></image></g><g></g></g></g></svg>')
+      .assert.value('input', '<svg width="360" height="360"><defs></defs><g></g><g><g visibility="visible"><g><path d="M0,0L360,0L360,120L0,120L0,0" fill="rgba(0,0,0,1)"></path><image xlink:href="../../image.png" x="0" y="0" width="100" height="100"></image></g><g></g></g><g visibility="visible"><g><path d="M0,120L360,120L360,170L0,170L0,120" fill="rgba(0,255,0,1)"></path><image xlink:href="../../image.png" x="0" y="120" width="100" height="100" transform="matrix(0.5,0,0,0.5,0,60)"></image></g><g></g></g><g visibility="visible"><g><path d="M0,170L50,170L50,290L0,290L0,170" fill="rgba(0,0,255,1)"></path><image xlink:href="../../image.png" x="0" y="170" width="100" height="100" transform="matrix(0.5,0,0,0.5,0,85)"></image></g><g></g></g></g></svg>')
       .end();
   }
 };
--- test/group2/img-mask-multi-svg/test.js
+++ test/group2/img-mask-multi-svg/test.js
@@ -6,7 +6,7 @@ module.exports = {
     browser
       .url('file://' + path.join(__dirname, 'index.html'))
       .waitForElementVisible('body', 1000)
-      .assert.value('input', '<defs><mask id="karas-defs-0-0"><path d="M0,0L100,0L50,100L0,0" fill="rgba(255,255,255,1)" stroke-width="0" transform="matrix(1,0,0,1,0,0)"></path></mask></defs><g></g><g><g visibility="visible" mask="url(#karas-defs-0-0)"><g></g><g><image xlink:href="../image.png" x="0" y="0" width="100" height="100"></image></g></g></g>')
+      .assert.value('input', '<defs><mask id="karas-defs-0-0"><path d="M0,0L100,0L50,100L0,0" fill="rgba(255,255,255,1)" stroke-width="0" transform="matrix(1,0,0,1,0,0)"></path></mask></defs><g></g><g><g visibility="visible" mask="url(#karas-defs-0-0)"><g></g><g><image xlink:href="../../image.png" x="0" y="0" width="100" height="100"></image></g></g></g>')
       .end();
   }
 };

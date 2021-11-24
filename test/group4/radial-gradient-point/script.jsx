4006485d461514cbcc7b19ff709ad68e449c8394
army8735 <army8735@qq.com>
2021-11-24 11:29:55 +0800

    Update script.jsx

--- test/group4/radial-gradient-point/script.jsx
+++ test/group4/radial-gradient-point/script.jsx
@@ -1,6 +1,6 @@
 karas.render(
   <canvas width="360" height="360">
-    <div style={{position:'absolute',left:50,top:20,width:100,height:200,background:'radial-gradient(0.6 0.1 0.7 0.5 0.3 0.2, #F00, #00F)'}}/>
+    <div style={{position:'absolute',left:50,top:20,width:100,height:200,background:'radial-gradient(0.6 0.1 0.7 0.5 0.3, #F00, #00F)'}}/>
   </canvas>,
   '#test'
 );

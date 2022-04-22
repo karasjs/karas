97a6b67675e46de56828b51643deb32cb24e59b6
army8735 <army8735@qq.com>
2022-04-22 09:46:13 +0800

    test: leading均分忘了在末尾

--- test/group3/inline-block-error-svg/test.js
+++ test/group3/inline-block-error-svg/test.js
@@ -6,7 +6,7 @@ module.exports = {
     browser
       .url('file://' + path.join(__dirname, 'index.html'))
       .waitForElementVisible('body', 1000)
-      .assert.value('input', '<defs></defs><g></g><g><g visibility="visible"><g><path d="M0,0L8.90625,0L8.90625,18.13671875L0,18.13671875L0,0" fill="rgba(255,0,0,1)"></path></g><g><g visibility="visible"><g></g><g><g><text x="0" y="14.484375" fill="rgba(0,0,0,1)" font-family="arial" font-weight="400" font-style="normal" font-size="16px">1</text></g></g></g></g></g></g>')
+      .assert.value('input', '<defs></defs><g></g><g><g visibility="visible"><g><path d="M0,0.26171875L8.90625,0.26171875L8.90625,18.13671875L0,18.13671875L0,0.26171875" fill="rgba(255,0,0,1)"></path></g><g><g visibility="visible"><g></g><g><g><text x="0" y="14.484375" fill="rgba(0,0,0,1)" font-family="arial" font-weight="400" font-style="normal" font-size="16px">1</text></g></g></g></g></g></g>')
       .end();
   }
 };
--- test/group3/inline-no-transform-svg/test.js
+++ test/group3/inline-no-transform-svg/test.js
@@ -6,7 +6,7 @@ module.exports = {
     browser
       .url('file://' + path.join(__dirname, 'index.html'))
       .waitForElementVisible('body', 1000)
-      .assert.value('input', '{"bb":[],"children":[{"bb":[{"type":"item","tagName":"path","props":[["d","M0,0L8.90625,0L8.90625,18.13671875L0,18.13671875L0,0"],["fill","rgba(255,0,0,1)"]]}],"children":[{"type":"text","children":[{"type":"item","tagName":"text","props":[["x",0],["y",14.484375],["fill","rgba(0,0,0,1)"],["font-family","arial"],["font-weight",400],["font-style","normal"],["font-size","16px"]],"content":"a"}]}],"visibility":"visible","type":"dom"}],"visibility":"visible","type":"dom","defs":[]}')
+      .assert.value('input', '{"bb":[],"children":[{"bb":[{"type":"item","tagName":"path","props":[["d","M0,0.26171875L8.90625,0.26171875L8.90625,18.13671875L0,18.13671875L0,0.26171875"],["fill","rgba(255,0,0,1)"]]}],"children":[{"type":"text","children":[{"type":"item","tagName":"text","props":[["x",0],["y",14.484375],["fill","rgba(0,0,0,1)"],["font-family","arial"],["font-weight",400],["font-style","normal"],["font-size","16px"]],"content":"a"}]}],"visibility":"visible","type":"dom"}],"visibility":"visible","type":"dom","defs":[]}')
       .end();
   }
 };

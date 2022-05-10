b08022467ef159a4a2e615fafbb648c8b9803834
army8735 <army8735@qq.com>
2022-05-10 19:40:31 +0800

    test: 垂直+居中

--- test/group4/text-align-svg/script.jsx
+++ test/group4/text-align-svg/script.jsx
@@ -3,6 +3,8 @@ let o = karas.render(
     <div style={{textAlign:'left'}}>1</div>
     <div style={{textAlign:'center'}}>1</div>
     <div style={{textAlign:'right'}}>1</div>
+    <div style={{textAlign:'center',width:100,height:100,background:'#F00',
+      writingMode:'verticalLr'}}>1</div>
   </svg>,
   '#test'
 );

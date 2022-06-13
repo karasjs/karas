b5e09d87452ae303ef07b694197ab80b9b0991cb
army8735 <army8735@qq.com>
2022-06-13 21:37:56 +0800

    test:

--- test/group3/flex-init-short/script.jsx
+++ test/group3/flex-init-short/script.jsx
@@ -7,6 +7,11 @@ karas.render(
     <div style={{display:'flex',justifyContent:'center'}}>
       <div style={{flex:0.5,background:'#00F'}}>a</div>
     </div>
+    <div style={{display:'flex',justifyContent:'center',width:300}}>
+      <div style={{display:'flex',flexDirection:'column',flex:1,background:'#F00'}}>
+        <span>a</span>
+      </div>
+    </div>
   </canvas>,
   '#test'
 );

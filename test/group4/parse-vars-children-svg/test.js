971cf6ec3f9d22d6f5c0b951f92c0b6cf9c77287
army8735 <army8735@qq.com>
2022-06-17 11:11:54 +0800

    test:

--- test/group4/parse-vars-children-svg/test.js
+++ test/group4/parse-vars-children-svg/test.js
@@ -7,7 +7,7 @@ module.exports = {
       .url('file://' + path.join(__dirname, 'index.html'))
       .waitForElementVisible('body', 1000)
       .pause(200)
-      .assert.value('input', '<defs></defs><g></g><g><g visibility="visible" transform="matrix(1,0,0,1,100,0)"><g></g><g><g><text x="0" y="14.484375" fill="rgba(0,0,0,1)" font-family="arial" font-weight="400" font-style="normal" font-size="16px">a</text></g></g></g><g visibility="visible"><g></g><g><g><text x="8.89844" y="14.484375" fill="rgba(0,0,0,1)" font-family="arial" font-weight="400" font-style="normal" font-size="16px">b</text></g></g></g></g>')
+      .assert.value('input', '<defs></defs><g></g><g><g visibility="visible" transform="matrix(1,0,0,1,100,0)"><g></g><g><g><text x="0" y="14.484375" fill="rgba(0,0,0,1)" font-family="arial" font-weight="400" font-style="normal" font-size="16px">a</text></g></g></g><g visibility="visible"><g></g><g><g><text x="8.90625" y="14.484375" fill="rgba(0,0,0,1)" font-family="arial" font-weight="400" font-style="normal" font-size="16px">b</text></g></g></g></g>')
       .end();
   }
 };

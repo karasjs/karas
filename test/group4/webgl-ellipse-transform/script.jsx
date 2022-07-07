211630634c0a6af127ccef7e201950d7663ca056
army8735 <army8735@qq.com>
2022-07-07 19:08:40 +0800

    test:

---
+++ test/group4/webgl-ellipse-transform/script.jsx
@@ -0,0 +1,16 @@
+let o = karas.render(
+  <webgl width="360" height="360">
+    <$ellipse style={{
+      position: 'absolute',
+      left: 180,
+      top: 180,
+      width: 100,
+      height: 50,
+      strokeWidth: 0,
+      background: '#F00',
+      fill: 'radial-gradient(farthest-side, rgba(0,0,0,0.5), rgba(0,0,0,0))',
+      translateX: '50%',
+    }}/>
+  </webgl>,
+  '#test'
+);

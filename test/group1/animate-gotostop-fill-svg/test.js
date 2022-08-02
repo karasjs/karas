139860769815bf7574492c448e9f6f995b77ca39
army8735 <army8735@qq.com>
2022-08-02 16:49:48 +0800

    test:

--- test/group1/animate-gotostop-fill-svg/test.js
+++ test/group1/animate-gotostop-fill-svg/test.js
@@ -7,7 +7,7 @@ module.exports = {
       .url('file://' + path.join(__dirname, 'index.html'))
       .waitForElementVisible('body', 1000)
       .pause(300)
-      .assert.value('input', '[{"k":"linear","d":180,"v":[[[127.5,127.5,0,1]],[[0,0,255,1]]]}]')
+      .assert.value('input', '[{"k":"linear","d":180,"v":[[[127,127,0,1]],[[0,0,255,1]]]}]')
       .end();
   }
 };

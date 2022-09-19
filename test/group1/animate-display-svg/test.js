b1b672dd61524a5977fd9a1e16a097964a1a4dfc
army8735 <army8735@qq.com>
2022-09-19 14:29:55 +0800

    test: 加些延迟

--- test/group1/animate-display-svg/test.js
+++ test/group1/animate-display-svg/test.js
@@ -6,7 +6,7 @@ module.exports = {
     browser
       .url('file://' + path.join(__dirname, 'index.html'))
       .waitForElementVisible('body', 1000)
-      .pause(250)
+      .pause(300)
       .assert.value('input', '{"bb":[],"children":[{"bb":[{"type":"item","tagName":"path","props":[["d","M0,0L100,0L100,100L0,100L0,0"],["fill","rgba(255,0,0,1)"]]}],"children":[{"type":"item","tagName":"path","props":[["d","M0,0L100,0L100,100L0,100L0,0"],["fill","none"],["stroke","rgba(0,0,0,1)"],["stroke-width",1]]}],"visibility":"visible","type":"geom"}],"visibility":"visible","type":"dom","defs":[],"lv":1}')
       .end();
   }

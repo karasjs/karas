8a21b214c68ba2ae69df5ea41efdaf3c9c88e49e
Army <army8735@qq.com>
2022-11-04 16:48:29 +0800

    fix: 精度问题

--- index.js
+++ index.js
@@ -32094,6 +32094,19 @@
 
     var paramA = closestParam(intersectPt, p1, p2);
     var paramB = closestParam(intersectPt, p3, p4);
+
+    if (paramA < 0 && Math.abs(paramA) < 1e-9) {
+      paramA = 0;
+    } else if (paramA > 1 && paramA - 1 < 1e-9) {
+      paramA = 1;
+    }
+
+    if (paramB < 0 && Math.abs(paramB) < 1e-9) {
+      paramB = 0;
+    } else if (paramB > 1 && paramB - 1 < 1e-9) {
+      paramB = 1;
+    }
+
     intersectPt.pa = paramA;
     intersectPt.pb = paramB;
 

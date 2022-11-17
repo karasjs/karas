bc120842771670075f8a55fe557abd698cec0e28
Army <army8735@qq.com>
2022-11-17 09:58:50 +0800

    test: fontSizeShrink时还要尝试没有ew的情况 #255

--- test/group3/font-size-shrink/script.jsx
+++ test/group3/font-size-shrink/script.jsx
@@ -1,10 +1,10 @@
 let o = karas.render(
   <canvas width={360} height={360}>
-    <div style={{width:50,whiteSpace:'nowrap',overflow:'hidden',
-      textOverflow:'ellipsis',fontSizeShrink:11}}>abcdefg</div>
-    <div style={{width:50,whiteSpace:'nowrap',overflow:'hidden',
+    <div style={{width:40,background:'#F00',whiteSpace:'nowrap',overflow:'hidden',
+      textOverflow:'ellipsis',fontSizeShrink:14}}>abcdefg</div>
+    <div style={{width:40,background:'#F00',whiteSpace:'nowrap',overflow:'hidden',
       textOverflow:'ellipsis',fontSizeShrink:10}}>abcdefg</div>
-    <div style={{width:50,whiteSpace:'nowrap',
+    <div style={{width:40,background:'#F00',whiteSpace:'nowrap',
       fontSizeShrink:10}}>abcdefg</div>
   </canvas>,
   '#test'

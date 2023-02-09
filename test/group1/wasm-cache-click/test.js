1ef9ce847a09c66929552cff0a6fd032c8fb4773
Army <army8735@qq.com>
2023-02-09 17:43:46 +0800

    test: #262

--- test/group1/wasm-cache-click/test.js
+++ test/group1/wasm-cache-click/test.js
@@ -6,7 +6,8 @@ module.exports = {
     browser
       .url('file://' + path.join(__dirname, 'index.html'))
       .waitForElementVisible('body', 1000)
-      .moveToElement('canvas', 51, 51)
+      .pause(20)
+      .moveToElement('canvas', 61, 61)
       .mouseButtonClick(0)
       .assert.value('input', '1')
       .end();
